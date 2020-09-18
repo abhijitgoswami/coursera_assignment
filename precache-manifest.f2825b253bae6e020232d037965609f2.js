124427c08c39ce815de5ce352f3e1ac4f83edb1d
abhijitgoswami <abhijeetgoswami45@gmail.com>
2020-09-18 14:15:12 +0530

    Updates

---
+++ precache-manifest.f2825b253bae6e020232d037965609f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e10c70d306f0694e5049df51ac0486b",
+    "url": "/coursera_assignment/learnreact/index.html"
+  },
+  {
+    "revision": "f467f7a77fea624178b2",
+    "url": "/coursera_assignment/learnreact/static/css/2.acd78e4d.chunk.css"
+  },
+  {
+    "revision": "ce02eb5530eb6b0d7148",
+    "url": "/coursera_assignment/learnreact/static/css/main.159289fb.chunk.css"
+  },
+  {
+    "revision": "f467f7a77fea624178b2",
+    "url": "/coursera_assignment/learnreact/static/js/2.7e2f3f10.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coursera_assignment/learnreact/static/js/2.7e2f3f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce02eb5530eb6b0d7148",
+    "url": "/coursera_assignment/learnreact/static/js/main.962c0350.chunk.js"
+  },
+  {
+    "revision": "b67d394d7894ff7070eb",
+    "url": "/coursera_assignment/learnreact/static/js/runtime-main.cb508096.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coursera_assignment/learnreact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coursera_assignment/learnreact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coursera_assignment/learnreact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coursera_assignment/learnreact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coursera_assignment/learnreact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
